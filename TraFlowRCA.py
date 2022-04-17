8fc8704fa30874ca80f835630a4ba79a5cc396cc
zt <zt17888803321@163.com>
2022-04-17 18:44:15 +0800

    version 1

--- TraFlowRCA.py
+++ TraFlowRCA.py
@@ -31,7 +31,7 @@ first_tag = True
 start_str = '2022-01-13 00:00:00'    # trace: '2022-02-25 00:00:00', span: '2022-01-13 00:00:00'
 init_start_str = '2022-01-13 00:00:00'    # normal traces  trace: '2022-02-25 00:00:00', span: '2022-01-13 00:00:00'
 window_duration = 60 * 60 * 1000    # ms
-AD_method = 'CEDAS_withscore'    # 'DenStream_withscore', 'DenStream_withoutscore', 'CEDAS_withscore', 'CEDAS_withoutscore'
+AD_method = 'DenStream_withscore'    # 'DenStream_withscore', 'DenStream_withoutscore', 'CEDAS_withscore', 'CEDAS_withoutscore'
 Sample_method = 'rate'    # 'none', 'micro', 'macro', 'rate'
 dataLevel = 'span'    # 'trace', 'span'
 path_decay = 0.01
@@ -194,11 +194,11 @@ def main():
     if AD_method in ['DenStream_withscore', 'DenStream_withoutscore']:
         # denstream = DenStream(eps=0.3, lambd=0.1, beta=0.5, mu=11)
         denstream = DenStream(eps=100, lambd=0.1, beta=0.2, mu=6)
-        # init_Cluster(denstream, init_start_str)
+        init_Cluster(denstream, init_start_str)
     elif AD_method in ['CEDAS_withscore', 'CEDAS_withoutscore']:
         cedas = CEDAS(r0=100, decay=0.001, threshold=5)
         first_tag = True
-        # init_Cluster(cedas, init_start_str)
+        init_Cluster(cedas, init_start_str)
 
     # ========================================
     # Init time window
